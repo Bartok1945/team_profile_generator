9f2993af8f3e420c3329c60a6d0d96ab94525d70
Bartok <thom.huenger@gmail.com>
2021-03-04 14:53:37 -0500

    fixed tests to make them work

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -5,17 +5,17 @@ const Employee = require("../lib/employee");
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+  expect(e.officeNum).toEqual(testValue);
 });
 
 test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
-  expect(e.getRole()).toBe(testValue);
+  expect(e.getRole()).toEqual(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+  expect(e.getOfficeNum()).toEqual(testValue);
 });
